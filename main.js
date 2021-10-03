1b45e3f564a045d1cdd885eb93ad5b86d3f07772
arthurGC <aguzmanc@unsa.edu.pe>
2021-10-03 14:16:52 -0500

    Add functionality to mobile menu in JS

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// This is a js file
+const navBar = document.querySelector('#desktop-nav');
+const hamburguerButton = document.querySelector('.menu-bars');
+const hamburguerIcon = document.querySelector('.navbar-icon');
+
+function doSomething() {
+  hamburguerIcon.classList.toggle('fa-bars');
+  hamburguerIcon.classList.toggle('fa-times');
+  navBar.classList.toggle('show-me');
+}
+
+hamburguerButton.addEventListener('click', doSomething);
