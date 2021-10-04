6fce690ac8944d82a46846c3d451d12175604f56
arthurGC <aguzmanc@unsa.edu.pe>
2021-10-04 00:16:35 -0500

    Fix name of funcion in JavaScript

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const navBar = document.querySelector('#desktop-nav');
 const hamburguerButton = document.querySelector('.menu-bars');
 const hamburguerIcon = document.querySelector('.navbar-icon');
 
-function doSomething() {
+function toggleMobileMenu() {
   hamburguerIcon.classList.toggle('fa-bars');
   hamburguerIcon.classList.toggle('fa-times');
   navBar.classList.toggle('show-me');
 }
 
-hamburguerButton.addEventListener('click', doSomething);
+hamburguerButton.addEventListener('click', toggleMobileMenu);
